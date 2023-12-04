25c065d04c46a420f5dd5a6e85632c570b4ade84
unknown <xlmriosx@gmail.com>
2023-12-04 20:53:38 -0300

    deploy database

--- app/Program.cs
+++ app/Program.cs
@@ -29,13 +29,19 @@ builder.Services.AddDbContext<ChatContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpRedirection();
 
 app.UseAuthorization();
 
